e6e9fefe07de573536364b91d122109ee26b0dee
许圣峰 <kricsleo@163.com>
2023-01-22 23:22:13 +0800

    feat: add unocss

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        🏗️
+      <main className='text-center'>
+        <div className='text-30'>🏗️</div>
+        <span className='text-6'>I'm working on it</span>
       </main>
     </>
   )
