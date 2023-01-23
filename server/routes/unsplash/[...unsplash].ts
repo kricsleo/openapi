5fc532f5fd74cbafa33d70fe2fd2f426c8819127
许圣峰 <kricsleo@163.com>
2023-01-23 23:04:07 +0800

    feat: add unsplash proxy

---
+++ server/routes/unsplash/[...unsplash].ts
@@ -0,0 +1,29 @@
+import { stringifyQuery } from 'ufo'
+import { createError } from 'h3'
+
+export default defineEventHandler(async event => {
+  const { unsplash } = event.context.params
+  const method = getMethod(event)
+  const queryStr = stringifyQuery(getQuery(event))
+  const headers = {
+    Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
+  }
+  const body = isMethod(event, 'GET') ? null : readRawBody(event, false) as unknown as Buffer
+  const response = await fetch(`https://api.unsplash.com/${unsplash}?${queryStr}`, {
+    method,
+    headers,
+    body
+  })
+  const data = await response.json()
+  console.log('response', response)
+  if(response.ok) {
+    return data
+  } else {
+    const error = createError({
+      statusCode: response.status,
+      statusMessage: response.statusText,
+      data
+    })
+    sendError(event, error)
+  }
+})
