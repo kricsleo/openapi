8fd7445fb0445ee02104a3caa4181b05a3fe6a0b
kricsleo <kricsleo@163.com>
2024-12-29 14:09:58 +0800

    fix: fix cors 3

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,21 +1,12 @@
+import { handleCors } from 'h3'
+
 export default defineEventHandler(async (event) => {
-  // Set CORS headers for all requests
-  setResponseHeaders(event, {
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
-    'Access-Control-Expose-Headers': '*',
-    'Access-Control-Max-Age': '31536000',
-    'Access-Control-Allow-Credentials': 'true'
+  const didHandleCors = handleCors(event, {
+    origin: '*',
+    preflight: {
+      statusCode: 204,
+    },
+    methods: '*',
   })
-
-  // Handle preflight requests
-  if (getMethod(event) === 'OPTIONS') {
-    event.node.res.statusCode = 204
-    event.node.res.end()
-    return
-  }
-
-  // Continue with the request
-  return
+  if(didHandleCors) return
 })
